634e2c472d86a7e3f3acb3c8a334366606515a9d
Luca Di Mauro <dima.91@live.it>
2022-04-11 17:40:24 +0200

    Defined "positioning_provider" components skeleton

---
+++ fleet-management-example/easyedge-firmware/components/positioning_provider/include/positioning_provider.h
@@ -0,0 +1,11 @@
+
+/**
+ * @file positioning_provider.h
+ * @author Luca Di Mauro (luca.dimauro@seco.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-11
+ */
+
+
+void func(void);
---
+++ fleet-management-example/easyedge-firmware/components/positioning_provider/positioning_provider.c
@@ -0,0 +1,15 @@
+
+/**
+ * @file positioning_provider.c
+ * @author Luca Di Mauro (luca.dimauro@seco.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-08
+ */
+
+
+#include <positioning_provider.h>
+
+void func(void) {
+
+}
