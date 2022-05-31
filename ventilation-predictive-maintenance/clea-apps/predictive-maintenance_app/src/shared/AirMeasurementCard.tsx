14d3477661983eeb4a34967967595f489af3b9ae
Bernardo Menicagli <bernardo.menicagli@gmail.com>
2022-05-31 18:26:09 +0200

    Add air pollution thresholds.
    Signed-off-by: Bernardo Menicagli <bernardo.menicagli@gmail.com>

--- ventilation-predictive-maintenance/clea-apps/predictive-maintenance_app/src/shared/AirMeasurementCard.tsx
+++ ventilation-predictive-maintenance/clea-apps/predictive-maintenance_app/src/shared/AirMeasurementCard.tsx
@@ -4,23 +4,49 @@ import Card from "./common/Card";
 
 import "./AirMeasurementCard.css";
 
+const getClassName = (value: number, thresholds: any): string => {
+  if (value < thresholds.warning) {
+    return "value mb-0 mt-3";
+  } else if (value <= thresholds.danger) {
+    return "value mb-0 mt-3 text-warning";
+  } else {
+    return "value mb-0 mt-3 text-danger";
+  }
+};
+
 type AirMeasurementCardProps = {
   title: string;
   unit: string;
   value: number;
+  thresholds?: {
+    warning: number;
+    danger: number;
+  };
   className?: string;
 };
 
-const AirMeasurementCard: React.FC<AirMeasurementCardProps> = ({ title, unit, value, className }) => {
-  return (
-    <Card className={className}>
-      <div className="d-flex">
-        <p className="title">{title}</p>
-        <p className="unit ms-auto">{unit}</p>
-      </div>
-      <p className="value mb-0 mt-3">{value.toFixed(2)}</p>
-    </Card>
-  );
+const AirMeasurementCard: React.FC<AirMeasurementCardProps> = ({ title, unit, value, thresholds, className }) => {
+  if (thresholds) {
+    return (
+      <Card className={className}>
+        <div className="d-flex">
+          <p className="title">{title}</p>
+          <p className="unit ms-auto">{unit}</p>
+        </div>
+        <p className={getClassName(value, thresholds)}>{value.toFixed(2)}</p>
+      </Card>
+    );
+  } else {
+    return (
+      <Card className={className}>
+        <div className="d-flex">
+          <p className="title">{title}</p>
+          <p className="unit ms-auto">{unit}</p>
+        </div>
+        <p className="value mb-0 mt-3">{value.toFixed(2)}</p>
+      </Card>
+    );
+  }
 };
 
 export default AirMeasurementCard;
